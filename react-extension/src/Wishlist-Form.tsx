009a10c02faba2c40f4341ace5c67cf409faf25e
noahnovickf <nnovickfaille@gmail.com>
2023-09-26 10:01:49 -0700

    style extension

--- react-extension/src/Wishlist-Form.tsx
+++ react-extension/src/Wishlist-Form.tsx
@@ -8,6 +8,9 @@ import {
 	Select,
 	Button,
     Image,
+    InputGroup,
+    InputLeftElement,
+    Flex,
 } from "@chakra-ui/react";
 import { Categories, Product } from "./App";
 import { useMutation } from "./hooks/use-query";
@@ -59,31 +62,64 @@ const WishlistForm = ({ product, categories = { categories: [] } }: WishlistProp
     console.log('FORMDATA', formData)
 
 	return (
-		<Box p={8}>
+		<Box p={4}>
 			<form onSubmit={handleSubmit}>
 				<FormControl>
-					<FormLabel>Title</FormLabel>
+					<FormLabel color={'scheme.main-green-blue'}>Title</FormLabel>
 					<Input
 						type="text"
 						name="title"
 						value={formData?.title}
 						onChange={handleChange}
+                        style={{
+                            color: '#1b243d',
+                            border: '1px solid #e5ebe7', // Customize the border color and style
+                            borderRadius: '0', // Optional: Remove any border-radius
+                            boxShadow: 'none', // Optional: Remove any box shadows
+                          }}
 					/>
 				</FormControl>
-				<FormControl mt={4}>
-					<FormLabel>Price</FormLabel>
+                <FormControl mt={4} color={'scheme.main-green-blue'}>
+					{formData?.image && (
+                        <Image
+                            mt={4} 
+                            src={formData.image}
+                            alt="Product Image"
+                            width={290} 
+                            height={290}
+                        />
+                    )}
+				</FormControl>
+				<FormControl mt={4} >
+					<FormLabel color={'scheme.main-green-blue'}>Price</FormLabel>
+                    <InputGroup>
+                    <InputLeftElement children="$"/>
 					<Input
+                        prefix="$"
 						type="text"
 						name="price"
 						value={formData?.price}
 						onChange={handleChange}
-					/>
+                        style={{
+                            color: '#1b243d',
+                            border: '1px solid #e5ebe7', // Customize the border color and style
+                            borderRadius: '0', // Optional: Remove any border-radius
+                            boxShadow: 'none', // Optional: Remove any box shadows
+                        }}
+                        />
+                        </InputGroup>
 				</FormControl>
 				<FormControl mt={4}>
-					<FormLabel>Category</FormLabel>
+					<FormLabel color={'scheme.main-green-blue'}>Category</FormLabel>
 					<Select
 						name="categoryId"
 						onChange={handleChange}
+                        style={{
+                            color: '#1b243d',
+                            border: '1px solid #e5ebe7', // Customize the border color and style
+                            borderRadius: '0', // Optional: Remove any border-radius
+                            boxShadow: 'none', // Optional: Remove any box shadows
+                          }}
 					>
                         {categories?.categories.map((category) =>
                             <option value={category.id}>{category.title}</option>
@@ -91,37 +127,46 @@ const WishlistForm = ({ product, categories = { categories: [] } }: WishlistProp
 					</Select>
 				</FormControl>
 				<FormControl mt={4}>
-					<FormLabel>Image URL</FormLabel>
-					{formData?.image && (
-                        <Image
-                            mt={4} // You can adjust the margin as needed
-                            src={formData.image}
-                            alt="Product Image"
-                            width={200} // Adjust the width as needed
-                            height={200} // Adjust the height as needed
-                        />
-                    )}
-				</FormControl>
-				<FormControl mt={4}>
-					<FormLabel>Description</FormLabel>
+					<FormLabel color={'scheme.main-green-blue'}>Description</FormLabel>
 					<Textarea
+                        placeholder="Add your own description"
 						name="description"
 						value={formData?.description}
 						onChange={handleChange}
+                        style={{
+                            color: '#1b243d',
+                            border: '1px solid #e5ebe7', // Customize the border color and style
+                            borderRadius: '0', // Optional: Remove any border-radius
+                            boxShadow: 'none', // Optional: Remove any box shadows
+                          }}
 					/>
 				</FormControl>
 				<FormControl mt={4}>
-					<FormLabel>Store</FormLabel>
+					<FormLabel color={'scheme.main-green-blue'}>Store</FormLabel>
 					<Input
 						type="text"
 						name="store"
 						value={formData?.store}
 						onChange={handleChange}
+                        style={{
+                            color: '#1b243d',
+                            border: '1px solid #e5ebe7', // Customize the border color and style
+                            borderRadius: '0', // Optional: Remove any border-radius
+                            boxShadow: 'none', // Optional: Remove any box shadows
+                          }}
 					/>
 				</FormControl>
-				<Button mt={4} colorScheme="teal" type="submit">
+                <Flex mt={4} justifyContent="space-between">
+				<Button mt={4} color='scheme.main-green-blue' type="submit">
 					Add to Wishlist
 				</Button>
+                <Button mt={4} color='scheme.main-green-blue' onClick={()=> {
+                    const home = `${process.env.REACT_APP_FRONTEND_URL}`;
+                    window.open(home, "_blank");
+                }}>
+                    Visit Genie
+                </Button>
+                </Flex>
 			</form>
 		</Box>
 	);
