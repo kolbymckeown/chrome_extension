1073f3180097eff82236684db21684b2b1529984
noahnovickf <nnovickfaille@gmail.com>
2023-10-16 15:24:06 -0700

    fix images

--- frontend/src/components/categories/category-card.tsx
+++ frontend/src/components/categories/category-card.tsx
@@ -95,7 +95,12 @@ export const CategoryCard = ({ category }: CategoryCardProps) => {
             {filteredImages.length > 3 ? (
               filteredImages.map((image: string, i: number) => (
                 <Box key={i} overflow="hidden" w={'130px'} h={'130px'}>
-                  <ImageWithFallback src={image} alt={`Image ${image}`} />
+                  <ImageWithFallback
+                    src={image}
+                    alt={`Image ${image} `}
+                    w="130px"
+                    h="130px"
+                  />
                 </Box>
               ))
             ) : (
@@ -109,6 +114,8 @@ export const CategoryCard = ({ category }: CategoryCardProps) => {
                 <ImageWithFallback
                   src={filteredImages[0]}
                   alt={`Image ${filteredImages[0]}`}
+                  w={'260px'}
+                  h={'260px'}
                 />
               </Box>
             )}
