654d385cbeb444f2963b5cd7152251af6a393479
Kolby <mckeownkolby@gmail.com>
2023-11-01 11:14:40 -0400

    updated store string + navbar

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -1,26 +1,32 @@
 import {
   Box,
-  Button,
   Flex,
-  Image,
-  Icon,
   Text,
-  Tooltip,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useDisclosure,
+  HStack,
+  Avatar,
+  Image,
 } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
 import useAuth from '@/hooks/use-auth';
 import { selectUser } from '@/redux/slices/user.slice';
 import { AddItem } from '../items/add-item';
 import { AddCategory } from '../categories/add-category';
-import { FaSignOutAlt } from 'react-icons/fa';
-import { useRouter } from 'next/router';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
   const user = useSelector(selectUser);
   const { logout } = useAuth();
   const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { pathname } = useLocation();
 
   const isLoginPage = pathname === '/session/login';
@@ -31,57 +37,52 @@ export default function Navbar() {
   };
 
   return (
-    <Box as="nav" px="4" bg="scheme.main-green-blue" boxShadow="sm">
-      <Flex h="20" alignItems="center" justifyContent="space-between" mx="auto">
-        <Link to="/">
-          <Image w="150px" h="50px" src="/genius-logo.png" alt="Genius Logo" />
-        </Link>
-        {user.email ? (
-          <>
-            <Flex display={{ base: 'none', md: 'flex' }} gap="4">
+    <Box bg="scheme.dusty-rose" px={4} shadow="lg">
+      <Flex h="20" alignItems="center" justifyContent="space-between">
+        <IconButton
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label="Open Menu"
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems="center">
+          <Link to="/">
+            <Image
+              w="150px"
+              h="50px"
+              src="/genius_white.png"
+              alt="Genius Logo"
+            />
+          </Link>
+          {user.email && (
+            <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
               <AddItem />
               <AddCategory />
-              {user?.firstName && <Text>Welcome, {user.firstName}.</Text>}
-
-              <Button
-                variant="outline"
-                color="scheme.dark-blue"
-                onClick={handleLogout}
-                border="none"
-                rightIcon={<FaSignOutAlt />}
-              >
-                Sign out
-              </Button>
-            </Flex>
-            <Flex display={{ base: 'flex', md: 'none' }} gap="8">
-              <AddItem variant="icon" />
-              <AddCategory variant="icon" />
-              <Tooltip label="Sign Out" aria-label="Sign Out">
-                <span>
-                  <Icon
-                    as={FaSignOutAlt}
-                    variant="outline"
-                    color="scheme.dark-blue"
-                    onClick={handleLogout}
-                    border="none"
-                    cursor={'pointer'}
-                    w={6}
-                    h={6}
-                  />
-                </span>
-              </Tooltip>
-            </Flex>
-          </>
-        ) : isLoginPage ? (
-          <Text fontWeight={'700'}>Welcome to Genius</Text>
-        ) : (
-          <Flex display={{ base: 'none', md: 'flex' }} gap="4">
-            <Link to="/session/login">
-              <Button color={'scheme.main-green-blue'}>Login</Button>
-            </Link>
-          </Flex>
-        )}
+            </HStack>
+          )}
+        </HStack>
+        <Flex alignItems="center">
+          {user.email ? (
+            <Button onClick={handleLogout}>Sign out</Button>
+          ) : (
+            !isLoginPage && (
+              <Link to="/session/login">
+                <Button color="scheme.main-green-blue">Login</Button>
+              </Link>
+            )
+          )}
+        </Flex>
       </Flex>
+
+      {isOpen && (
+        <Box pb={4} display={{ md: 'none' }}>
+          <HStack as="nav" spacing={4}>
+            <AddItem />
+            <AddCategory />
+          </HStack>
+        </Box>
+      )}
     </Box>
   );
 }
